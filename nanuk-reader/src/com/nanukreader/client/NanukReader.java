4e5b4f9468dde7ad75bb5a718df9348854561a5c
michaellif <michael.lifschitz@gmail.com>
2015-01-31 18:06:41 -0500

    load simple text file from the same origin

--- nanuk-reader/src/com/nanukreader/client/NanukReader.java
+++ nanuk-reader/src/com/nanukreader/client/NanukReader.java
@@ -1,6 +1,12 @@
 package com.nanukreader.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -9,9 +15,40 @@ import com.google.gwt.user.client.ui.RootPanel;
  */
 public class NanukReader implements EntryPoint {
 
+    private HTML viewer;
+
     @Override
     public void onModuleLoad() {
-        final Label errorLabel = new Label("TEST");
-        RootPanel.get().add(errorLabel);
+        viewer = new HTML();
+        RootPanel.get().add(viewer);
+
+        getEpub("http://localhost:8888/test.epub");
+
+    }
+
+    private void getEpub(String url) {
+        RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
+
+        try {
+            builder.sendRequest(null, new RequestCallback() {
+                @Override
+                public void onError(Request request, Throwable exception) {
+                    System.err.println("Couldn't retrieve file");
+                }
+
+                @Override
+                public void onResponseReceived(Request request, Response response) {
+                    if (200 == response.getStatusCode()) {
+                        viewer.setText(response.getText());
+                        System.out.println(response.getText());
+                    } else {
+                        System.err.println("Couldn't retrieve file (" + response.getStatusText() + ")");
+                    }
+                }
+            });
+        } catch (RequestException e) {
+            System.err.println("Couldn't retrieve file");
+        }
+
     }
 }
