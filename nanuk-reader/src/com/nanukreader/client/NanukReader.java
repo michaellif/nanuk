9d3be38bf4f1051ae0c9f95acafe80f7bb148a17
michaellif <michael.lifschitz@gmail.com>
2015-02-08 21:37:37 -0500

    cleanup

--- nanuk-reader/src/com/nanukreader/client/NanukReader.java
+++ nanuk-reader/src/com/nanukreader/client/NanukReader.java
@@ -4,15 +4,12 @@ import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JsonUtils;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.dom.client.FrameElement;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
