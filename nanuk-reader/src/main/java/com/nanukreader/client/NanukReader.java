6d41a1fcfb7f51020898a14b3a57e080f272d052
michaellif <michael.lifschitz@gmail.com>
2015-02-18 18:58:38 -0500

    Load books toolbar

--- nanuk-reader/src/main/java/com/nanukreader/client/NanukReader.java
+++ nanuk-reader/src/main/java/com/nanukreader/client/NanukReader.java
@@ -11,6 +11,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.nanukreader.client.bookviewer.IBookViewer;
@@ -25,75 +26,48 @@ public class NanukReader implements EntryPoint {
 
     private static final Logger logger = Logger.getLogger(NanukReader.class.getName());
 
+    private HTML packagingDescriptorViewer;
+
+    private Image coverViewer;
+
+    private HTML navViewer;
+
+    private IBookViewer bookViewer;
+
     @Override
     public void onModuleLoad() {
         FlowPanel contentPanel = new FlowPanel();
         RootPanel.get().add(contentPanel);
 
-        final HTML packagingDescriptorViewer = new HTML();
+        packagingDescriptorViewer = new HTML();
         packagingDescriptorViewer.getElement().getStyle().setPadding(20, Unit.PX);
 
-        final Image coverViewer = new Image();
+        coverViewer = new Image();
 
-        final HTML navViewer = new HTML();
+        navViewer = new HTML();
 
-        final IBookViewer bookViewer = new DevBookViewer();
+        bookViewer = new DevBookViewer();
 
-        Button loadButton = new Button("Load", new ClickHandler() {
+        HorizontalPanel loadToolbar = new HorizontalPanel();
 
+        Button loadMobyDickButton = new Button("Load Moby Dick", new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-
-                // final String url = "http://127.0.0.1:8899/wasteland.epub";
-
-                final String url = "http://127.0.0.1:8899/moby-dick.epub";
-
-                Librarian.instance().addBook(url, new AsyncCallback<Book>() {
-
-                    @Override
-                    public void onFailure(Throwable caught) {
-                        throw new Error(caught);
-                    }
-
-                    @Override
-                    public void onSuccess(final Book book) {
-                        packagingDescriptorViewer.setText(JsonUtils.stringify(book.getPackagingDescriptor()));
-
-                        book.getContentItem(book.getPackagingDescriptor().getCoverImageItem().getId(), new AsyncCallback<String>() {
-
-                            @Override
-                            public void onFailure(Throwable caught) {
-                                // TODO Auto-generated method stub
-                                throw new Error(caught);
-                            }
-
-                            @Override
-                            public void onSuccess(String content) {
-                                coverViewer.setUrl("data:image/png;base64," + content);
-                            }
-                        });
-
-                        book.getContentItem(book.getPackagingDescriptor().getNavItem().getId(), new AsyncCallback<String>() {
-
-                            @Override
-                            public void onFailure(Throwable caught) {
-                                // TODO Auto-generated method stub
-                                throw new Error(caught);
-                            }
-
-                            @Override
-                            public void onSuccess(String content) {
-                                navViewer.setHTML(content);
-                            }
-                        });
-
-                        bookViewer.openBook(book);
-                    }
-                });
+                openbook("http://127.0.0.1:8899/moby-dick.epub");
             }
         });
+        loadToolbar.add(loadMobyDickButton);
 
-        contentPanel.add(loadButton);
+        Button loadWastelandButton = new Button("Load Wasteland", new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                openbook("http://127.0.0.1:8899/wasteland.epub");
+            }
+        });
+        loadToolbar.add(loadWastelandButton);
+
+        contentPanel.add(loadToolbar);
+
         contentPanel.add(bookViewer);
         contentPanel.add(packagingDescriptorViewer);
         contentPanel.add(coverViewer);
@@ -101,4 +75,48 @@ public class NanukReader implements EntryPoint {
 
     }
 
+    void openbook(String url) {
+        Librarian.instance().addBook(url, new AsyncCallback<Book>() {
+
+            @Override
+            public void onFailure(Throwable caught) {
+                throw new Error(caught);
+            }
+
+            @Override
+            public void onSuccess(final Book book) {
+                packagingDescriptorViewer.setText(JsonUtils.stringify(book.getPackagingDescriptor()));
+
+                book.getContentItem(book.getPackagingDescriptor().getCoverImageItem().getId(), new AsyncCallback<String>() {
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        // TODO Auto-generated method stub
+                        throw new Error(caught);
+                    }
+
+                    @Override
+                    public void onSuccess(String content) {
+                        coverViewer.setUrl("data:image/png;base64," + content);
+                    }
+                });
+
+                book.getContentItem(book.getPackagingDescriptor().getNavItem().getId(), new AsyncCallback<String>() {
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        // TODO Auto-generated method stub
+                        throw new Error(caught);
+                    }
+
+                    @Override
+                    public void onSuccess(String content) {
+                        navViewer.setHTML(content);
+                    }
+                });
+
+                bookViewer.openBook(book);
+            }
+        });
+    }
 }
